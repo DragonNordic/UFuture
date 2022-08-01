6b59852cc32737249140ad220e1274ce550f80f2
DragonNordic <nordik.commerce@gmail.com>
2022-08-01 16:10:13 +0300

    Project fixes and Document page implementation

--- dist/script.js
+++ dist/script.js
@@ -8,38 +8,4 @@ ToggleBtnId.addEventListener("click", () => {
 
 CloseBtnId.addEventListener("click", () => {
   navId.classList.remove("show");
-});
-
-AOS.init();
-
-gsap.from(".nav__logo", {
-  opacity: 0,
-  y: -10,
-  delay: 1,
-  duration: 0.5,
-});
-
-gsap.from(".nav__menu-list .nav__item", {
-  opacity: 0,
-  y: -10,
-  delay: 1.4,
-  duration: 0.5,
-  stagger: 0.3,
-});
-
-gsap.from(".nav__toggle-btn", {
-  opacity: 0,
-  y: -10,
-  delay: 1.4,
-  duration: 0.5,
-});
-
-/* ==== HEADER ==== */
-
-gsap.from(".footer__headling", {
-  opacity: 0,
-  y: -10,
-  delay: 1,
-  duration: 0.5,
-});
-  
+});  
